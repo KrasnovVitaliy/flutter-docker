7b023e63614dbd7fd4fed7e5169cef36a6d6629b
Vitaliy Krasnov <krasnov.v.i@gmail.com>
2024-10-11 02:11:08 +0300

    Update flutter

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.2-stable.tar.xz
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
 # docker buildx build --platform linux/amd64 -t flutter:latest ./ # MacOS ARM
 docker build -t flutter:latest ./
 docker tag flutter:latest krasnovvi/flutter:latest
 
-docker login -p ******** -u ********
+docker login -p ******* -u *******
 docker push krasnovvi/flutter:latest
--- dockerfile
+++ dockerfile
@@ -1,3 +1,3 @@
 FROM cirrusci/flutter
-ADD flutter_linux_3.22.2-stable.tar.xz /sdks
+ADD flutter_linux_3.24.3-stable.tar.xz /sdks
 RUN git config --global --add safe.directory /sdks/flutter
