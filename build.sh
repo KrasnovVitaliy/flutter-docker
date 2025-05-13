7793e8a54bdd5bda0da738d99be3f3c08e51b730
Vitaliy Krasnov <krasnov.v.i@gmail.com>
2025-05-14 02:47:17 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.3-stable.tar.xz
 # docker buildx build --platform linux/amd64 -t flutter:latest ./ # MacOS ARM
-docker build -t flutter:latest ./
-docker tag flutter:latest krasnovvi/flutter:latest
+docker build -t flutter:3.29.3 ./
+docker tag flutter:3.29.3 krasnovvi/flutter:3.29.3
+docker tag flutter:3.29.3 krasnovvi/flutter:latest
 
-docker login -p ******* -u *******
+docker login -p **** -u ****
+docker push krasnovvi/flutter:3.29.3
 docker push krasnovvi/flutter:latest
